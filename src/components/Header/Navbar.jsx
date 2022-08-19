168294213322f40e80e9d55a873e60d2e0c0a876
Gene Flores <goflores@usc.edu>
2022-08-19 12:40:21 -0700

    Mac edits

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -3,11 +3,7 @@ function Navbar() {
         <nav class="header__navbar navbar navbar-expand-lg animate__animated animate__slideInDown">
             <div class="container-fluid">
                 <a class="navbar-brand" href="/">Logo</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ms-auto">
+                <ul class="navbar-nav ms-auto">
                         <li class="header__navbar__nav-item">
                             <a class="nav-link active" aria-current="page" href="/">About</a>
                         </li>
@@ -15,7 +11,6 @@ function Navbar() {
                             <a class="nav-link" href="/">Contact Us</a>
                         </li>
                     </ul>
-                </div>
             </div>
         </nav>
     );
