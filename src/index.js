17baabea4acb5c65675f3d632171933b78c28031
Gene Flores <goflores@usc.edu>
2024-04-24 12:38:09 -0700

    sitemap

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {Route, Routes, Navigate} from "react-router-dom"
+import { BrowserRouter as Router} from "react-router-dom";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // root.render(
@@ -12,5 +15,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
-    <App />
+    <React.StrictMode>
+        <Router>
+            <Routes>
+                <Route path="/" element={<App />} />
+                <Route path="/sitemap.xml" element={<Navigate to="/sitemap.xml" />} />
+                <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+        </Router>
+    </React.StrictMode>
 );
